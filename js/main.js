7acd8aeb74dd8ffcddcfaabd020164041a932f76
dapwar <isast1984@gmail.com>
2023-07-07 07:12:53 -0500

    cambios en el menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const burguer = document.querySelector('#burguer');
 const enlaces = document.querySelector('#menuDesktop');
-   /*menu = document.querySelector('menuMobile div:nth-child(2)');*/
-    burguer.addEventListener('click', () => {
-         enlaces.classList.toggle('active');
+   menu = document.querySelector('#menuMobile div:nth-child(2)');
+    burguer.addEventListener('click', e=>{
+         burguer.classList.toggle('active');
+         menu.classList.toggle('open');
 });
-
-//''
